98e3cef011c0509a894efd0d2ce63b97849e70e9
liu-hz18 <1456437967@qq.com>
2020-09-29 00:40:59 +0800

    add antlr4 in requirement

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "这里面放你在运行前要跑的命令，比如运行 parser generator 等。"
 pip install -r minidecaf/requirements.txt
+java -jar /usr/local/lib/antlr-4.8-complete.jar -Dlanguage=Python3 -visitor -o minidecaf/generated minidecaf/Expr.g4
