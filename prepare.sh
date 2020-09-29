ba3d66f97028ff400eb4e1a4eb0a0e31b82c377f
liu-hz18 <1456437967@qq.com>
2020-09-29 10:46:31 +0800

    add parallel in requirements

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "这里面放你在运行前要跑的命令，比如运行 parser generator 等。"
+apt install parallel
 pip install -r minidecaf/requirements.txt
 java -jar /usr/local/lib/antlr-4.8-complete.jar -Dlanguage=Python3 -visitor -o minidecaf/generated minidecaf/Expr.g4
